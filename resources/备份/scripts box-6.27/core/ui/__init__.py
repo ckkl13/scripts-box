24d84e6223bb456331712fc5d41279726af7916a
ck <130759850+ckkl13@users.noreply.github.com>
2026-07-03 16:01:39 +0800

    Update 20260703

---
+++ resources/备份/scripts box-6.27/core/ui/__init__.py
@@ -0,0 +1,21 @@
+# 确保 qt_compat 先被加载
+from core.qt_compat import *
+
+from .widgets import DraggableGroupButton, MiddleClickButton
+from .recycle_bin_ui import RecycleBinUI
+from .group_panel import GroupPanel
+from .tool_button_manager import ToolButtonManager
+from .settings_dialog import SettingsDialog
+from .help_dialog import HelpDialog
+from .search_results_ui import SearchResultsUI
+
+__all__ = [
+    'DraggableGroupButton',
+    'MiddleClickButton',
+    'RecycleBinUI',
+    'GroupPanel',
+    'ToolButtonManager',
+    'SettingsDialog',
+    'HelpDialog',
+    'SearchResultsUI'
+]
